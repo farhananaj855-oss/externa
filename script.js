c5a636b82edbc638d80201905b11d237f12c3df0
FARHAN ISRAQ 5B27 <farhananaj855@gmail.com>
2025-08-13 19:18:50 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let car =['jack','hhh','lll','hh',"jjj","loo"]
+for(i=0;i<=5;i++){
+console.log(car[i])
+}
+for(j=1;j<=10;j=j+2){
+console.log(j)
+}
+car.map(function(x){
+    console.log(x)
+})
+let number=[1,2,3]
+number.map(function(items)
+{console.log(items+10)
+
+})
